ee91e649499064a6cc761c606d53ba947bd2fea6
Tushar22303 <tprajapati22303@gmail.com>
2024-06-29 21:48:33 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import DataFetching from './Components/Use-Effect Hook/DataFetching';
 import UseEffect_CleanUp from './Components/Use-Effect Hook/UseEffect_CleanUp';
 import DataFetching_2 from './Components/Use-Effect Hook/DataFetching_2';
 import Navbar from './Components/Use-Effect Hook/Navbar/Navbar';
+import Header from './Components/Use-Effect Hook/Header/Header';
 
 function App() {
   return (
     <>
-      <Navbar />
+      <Header />
     </>
   );
 }
